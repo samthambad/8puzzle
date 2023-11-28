8722c66bffd0f82aa6a3cdc61394b7cfd71eab95
sam <thambad@gmail.com>
2023-11-28 12:51:51 +0800

    Added comparator

--- Solver.java
+++ Solver.java
@@ -1,5 +1,6 @@
 import edu.princeton.cs.algs4.MinPQ;
 
+import java.util.Comparator;
 import java.util.Iterator;
 
 public class Solver {
@@ -15,14 +16,28 @@ public class Solver {
             this.prevNode = prevNode;
         }
     }
+    private Comparator<Node> pOrder() {
+        Comparator<Node> nodeComparator = new Comparator<Node>() {
+            public int compare(Node n1, Node n2) {
+                if (n1.board.manhattan()+n1.moves < n2.board.manhattan()+n2.moves) {
+                    return 1;
+                }
+                else if(n1.board.manhattan()+n1.moves > n2.board.manhattan()+n2.moves) {
+                    return -1;
+                }
+                else return 0;
+            }
+        };
+        return nodeComparator;
+    }
     public Solver(Board initial){
         // node = board, num of moves to reach board, previous node
         Node first = new Node(initial, 0, null);
-        MinPQ<Node> pq = new MinPQ<>();
+        MinPQ<Node> pq = new MinPQ<>(pOrder());
         pq.insert(first);
         int numMoves = 0;
         while(true){
-            Node parent = pq.delMin();
+            Node parent = pq.delMin(); // how to sort according to priority = moves + manhattan?
             if(parent.board.hamming() == 0){
                 break;
             }
